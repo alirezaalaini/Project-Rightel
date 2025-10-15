56eb76f2dc8647c746ce495b88935d5d9c6e8a40
alireza alaini <alirezaalaini7@gmail.com>
2025-10-15 20:42:09 +0330

    create swiper two

---
+++ swiper.js
@@ -0,0 +1,8 @@
+    var swiper = new Swiper(".mySwiper-two", {
+      slidesPerView: 3.2,
+      spaceBetween: 30,
+            navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
