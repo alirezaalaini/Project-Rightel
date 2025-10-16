e92a201e6bfb09cdb70acd769f83af06ccb8b282
AliJokari <alijokary2026@gmail.com>
2025-10-16 19:16:41 +0330

    Create Animate

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ const button = document.getElementById("infoButton");
     icon.classList.remove("opacity-0");
     icon.classList.add("opacity-100");
   });
+
+   function showPage(id, btn) {
+  
+  document.getElementById('demo1').classList.add('hidden');
+  document.getElementById('demo2').classList.add('hidden');
+
+  
+  document.getElementById(id).classList.remove('hidden');
+
+  document.querySelectorAll('.active-tab').forEach(el => el.classList.remove('active-tab', 'bg-[#E9E1E8]'));
+
+  btn.classList.add('active-tab', 'bg-[#E9E1E8]');
+}
